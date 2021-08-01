b3259445adb1189b68d7b35490ca10ea98361cfa
russibc <bruna.cr@aluno.ifsc.edu.br>
2021-08-01 14:50:24 -0300

    Code fix

--- dht_read.ino
+++ dht_read.ino
@@ -1,18 +1,31 @@
 #include <DHT.h>
 
 #define DHTPIN 5 // D1
-#define DHTTYPE DHT11 
+#define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup(){
+void setup() {
   Serial.begin(115200);
+  dht.begin();
 }
 
-void loop(){
-  Serial.println("Temperature:");
-  Serial.println(dht.readTemperature());
-  Serial.print("Humidity:");
-  Serial.println(dht.readHumidity());
-  delay(1000);
+void loop() {
+
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+
+  if (isnan(t) || isnan(h))
+  {
+    Serial.println("Failed to read from DHT");
+  }
+  else
+  {
+    Serial.println("Temperature:");
+    Serial.println(t);
+    Serial.print("Humidity:");
+    Serial.println(h);
+    delay(1000);
+  }
+
 }
